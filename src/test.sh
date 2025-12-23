e0093fcadca675882ccf8d544c938957ab94b196
miv403 <miv403@duck.com>
2025-12-24 00:12:30 +0300

    fixed typo in the echo statement

--- src/test.sh
+++ src/test.sh
@@ -6,7 +6,8 @@ set -e
 echo "Creating 100MB random file..."
 dd if=/dev/urandom of=test.bin bs=1M count=100 status=progress
 
-echo "\nRunning hill cipher..."
+echo ""
+echo "Running hill cipher..."
 # Assuming 'make run' handles the execution via qemu as configured
 make run
 
